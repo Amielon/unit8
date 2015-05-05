e2f8acca9bf3bc1db508a9da9d20da68a27af621
Ami <amiel.lopez@hotmail.com>
2015-05-05 15:47:15 +0200

    Nombre clase cambiado y metodo ToString

---
+++ ED_U8/src/unit8/Persona.java
@@ -0,0 +1,36 @@
+package unit8;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Ami
+ */
+public class Persona {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    protected int x;
+    protected String b;
+
+    public boolean verdad()
+    {
+        return true;
+    }
+    
+    public String ToString()
+    {
+        return b;
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
