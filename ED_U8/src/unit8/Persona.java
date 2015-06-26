30b683c3df746ede4d2b1c78657afaff811dd2d5
Ami <amiel.lopez@hotmail.com>
2015-06-26 11:34:05 +0200

    Añadido Persona,Profesor,Alumno

--- ED_U8/src/unit8/Persona.java
+++ ED_U8/src/unit8/Persona.java
@@ -1,5 +1,10 @@
 package unit8;
 
+import static java.lang.System.out;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,28 +14,65 @@ package unit8;
  *
  * @author Ami
  */
-public class Persona {
-
-    /**
-     * @param args the command line arguments
-     */
+public abstract class Persona {
     
-    protected int x;
-    protected String b;
-
-    public boolean verdad()
+    protected String nombre;
+    protected String dni;
+    protected ArrayList <Asignatura> Asignaturas;
+    
+    public Persona(String nombre, String dni)
     {
-        return true;
+        this.nombre = nombre;
+        this.dni = dni;
+        Asignaturas = new ArrayList();
     }
-    
-    public void ToString()
+    public void AnyadirAsignatura(Asignatura A)
     {
-        System.out.print(b + x);
+        Asignaturas.add(A);
     }
-    
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public void OrdenarporAsignatura()
+    {
+      //No se ha explicado en clase, no idea
+    }
+    public void ordenarporNota()
+    {
+        //Ni idea
+    }
+    public void mostrarListadoAsignaturasNota()
+    {
+        int longitud = Asignaturas.size();
         
+        for (int c = 0; c < longitud ; c++)
+        {
+            out.println("Nombre de la persona: " + nombre);
+            out.println("Asignatura: " + Asignaturas.get(c).getNombre());
+            out.println("Nota: " + Asignaturas.get(c).getNota());
+        }
+    }
+    public abstract boolean Esprofesor();
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public ArrayList<Asignatura> getAsignaturas() {
+        return Asignaturas;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setAsignaturas(ArrayList<Asignatura> Asignaturas) {
+        this.Asignaturas = Asignaturas;
     }
     
 }
